bf7d260b4c45d0813ee5d02708e8e3e9ef5c7ee0
Michael Neumann <mneumann@ntecs.de>
2015-03-24 21:58:31 +0100

    Use fixed-point arith for time. Add use of Priority Queue

--- snn.rs
+++ snn.rs
@@ -1,10 +1,15 @@
 use std::num::Float;
+use std::collections::binary_heap::BinaryHeap; // pq
+use std::cmp::Ordering;
 
 type float = f64;
+type time = u64;
+
+const TIME_RESOLUTION: time = 1000_000; // in micro seconds.
 
 #[derive(Debug)]
 struct Config {
-    arp:       float,
+    arp:       time,
     tau_m:     float,
     tau_r:     float,
     weight_r:  float,
@@ -14,14 +19,14 @@ struct Config {
 #[derive(Debug)]
 struct Neuron {
     /// End of absolute refractory period 
-    arp_end:         float,
-    last_spike_time: float,
+    arp_end:         time,
+    last_spike_time: time,
     mem_pot:         float,
 }
 
 #[derive(Debug)]
 enum NeuronResult {
-    Arp,
+    InArp,
     NoFire,
     Fire
 }
@@ -29,23 +34,26 @@ enum NeuronResult {
 impl Neuron {
     fn new() -> Neuron {
         Neuron {
-            arp_end: 0.0,
-            last_spike_time: 0.0,
+            arp_end: 0,
+            last_spike_time: 0,
             mem_pot: 0.0
         }
     }
 
-    fn spike(&mut self, timestamp: float, weight: float, cfg: &Config) -> NeuronResult {
-        assert!(timestamp >= 0.0);
-        debug_assert!(self.last_spike_time >= 0.0);
+    fn spike(&mut self, timestamp: time, weight: float, cfg: &Config) -> NeuronResult {
+        assert!(timestamp >= self.last_spike_time);
 
+        // Return early if still in absolute refractory period (arp)
+        if timestamp < self.arp_end {
+            return NeuronResult::InArp;
+        }
+         
         // Time since end of last absolute refractory period (arp)
+        assert!(timestamp >= self.arp_end);
         let delta = timestamp - self.arp_end;
 
-        // Return early if still in arp
-        if delta < 0.0 {
-            return NeuronResult::Arp;
-        }
+        // Convert delta to float
+        let delta: float = (delta as float) / TIME_RESOLUTION as float;
 
         // Calculate dynamic threshold
         let dyn_threshold = cfg.threshold +
@@ -53,7 +61,11 @@ impl Neuron {
 
         // Update memory potential
         if cfg.tau_m > 0.0 {
+          
             let d = timestamp - self.last_spike_time;
+            // Convert delta to float
+            let d: float = (d as float) / TIME_RESOLUTION as float;
+
             let decay = (-d / cfg.tau_m).exp();
             self.mem_pot *= decay;
             self.mem_pot += weight;
@@ -75,10 +87,42 @@ impl Neuron {
     }
 }
 
+#[derive(Debug)]
+struct Event {
+    time:   time,
+    weight: float,
+    target: u64
+}
+
+impl PartialEq for Event {
+    fn eq(&self, other: &Self) -> bool {
+        self.time == other.time
+    }
+}
+
+impl Eq for Event {}
+
+impl PartialOrd for Event {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.time.partial_cmp(&other.time).map(|o| o.reverse())
+    }
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.time.cmp(&other.time).reverse()
+    }
+}
+
+
+fn ms(n: time) -> time { n * 1000 }
+fn us(n: time) -> time { n }
+
 fn main() {
+   
     // Koinzidenz neurons
     let cfg_k = Config {
-        arp: 0.5,
+        arp: us(500), // 0.5 ms = 500 us
         tau_m: 0.04,
         tau_r: 0.5,
         weight_r: 0.0,
@@ -87,7 +131,7 @@ fn main() {
 
     // Input neurons
     let cfg_i = Config {
-        arp: 1.0,
+        arp: ms(1),
         tau_m: 0.0,
         tau_r: 0.5,
         weight_r: 0.0,
@@ -95,14 +139,35 @@ fn main() {
     };
 
     let mut n1 = Neuron::new();
+    let mut neurons = vec![n1];
+    let mut neurons = neurons.as_mut_slice();
 
+    let mut pq: BinaryHeap<Event> = BinaryHeap::new();
+
+    // Fill event queue with events
     for i in 1..100 {
-        let t = i as float / 10.0;
-        println!("-------------------------------------");
-        println!("t: {}", t);
+        let ev = Event {
+            time:   ms(i) / 10,   
+            weight: 0.6,
+            target: 0
+        }; 
 
-        let fire = n1.spike(t, 0.5, &cfg_i);
-        println!("{:?}", fire);
-        println!("{:?}", n1);
+        pq.push(ev);
     }
+
+    loop {
+        println!("-------------------------------------");
+
+        if let Some(ev) = pq.pop() {
+            println!("{:?}", ev);
+            let neuron = &mut neurons[ev.target as usize];
+            let fire = neuron.spike(ev.time, ev.weight, &cfg_i);
+            println!("{:?}", fire);
+            println!("{:?}", neuron);
+        }
+        else {
+            break;
+        }
+    }
+
 }
