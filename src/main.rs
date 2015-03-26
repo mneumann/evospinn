30009bc463d090c2bf1673a895332b77d7f3e081
Michael Neumann <mneumann@ntecs.de>
2015-03-26 23:27:53 +0100

    Add example net (Jeffress) from Johannes diploma thesis

--- src/main.rs
+++ src/main.rs
@@ -176,6 +176,17 @@ impl Net {
         self.events.push(event);
     }
 
+    // values are in ms
+    fn add_spike_train_float_ms(&mut self, target: NeuronId, weight: float, spikes: &[float]) {
+        for &t in spikes {
+            self.add_event(Event {
+                time: (t * 1000.0) as time, // convert to us
+                weight: weight,
+                target: target
+            });
+        }
+    }
+
     fn fire(&mut self, timestamp: time, neuron_id: NeuronId) {
         for syn in &self.synapses[neuron_id.0] {
             println!("{:?}", syn);
@@ -234,55 +245,120 @@ impl Net {
     }
 }
 
+// From Optimierung-1/stimuli.txt
 
+const spikes_input_0: [float; 49] = [
+    0.38, 3.38, 6.38, 9.38, 12.38, 15.38, 18.38,
+    21.38, 24.38, 27.38, 30.38, 33.38, 36.38, 39.38,
+    42.38, 45.38, 48.38, 52.98, 55.98, 58.98,
+    61.98, 64.98, 67.98, 70.98, 73.98, 76.98, 79.98,
+    82.98, 85.98, 88.98, 91.98, 94.98, 97.98,
+    103.59, 106.59, 109.59, 112.59, 115.59, 118.59,
+    121.59, 124.59, 127.59, 130.59, 133.59, 136.59, 139.59,
+    142.59, 145.59, 148.59
+];
+
+const spikes_input_1: [float; 49] = [
+    1.0, 4.0, 7.0, 10.0, 13.0, 16.0, 19.0,
+    22.0, 25.0, 28.0, 31.0, 34.0, 37.0,
+    40.0, 43.0, 46.0, 49.0, 53.0, 56.0, 59.0,
+    62.0, 65.0, 68.0, 71.0, 74.0, 77.0,
+    80.0, 83.0, 86.0, 89.0, 92.0, 95.0, 98.0,
+    103.0, 106.0, 109.0, 112.0, 115.0, 118.0,
+    121.0, 124.0, 127.0, 130.0, 133.0, 136.0, 139.0,
+    142.0, 145.0, 148.0
+];
+
+const correct_output_0: (float, float) = (  0.0,  47.0);
+const correct_output_1: (float, float) = ( 47.0, 100.0);
+const correct_output_2: (float, float) = (100.0, 170.0);
 
 fn main() {
     let mut net = Net::new();
 
     let input_neuron_template = NeuronConfig {
-        arp: ms(1),
+        arp: ms(0),
         tau_m: 0.0,
-        tau_r: 0.5,
+        tau_r: 0.0,
         weight_r: 0.0,
-        threshold: 0.6,
-        record: None
+        threshold: 0.0,
+        record: Some("input"),
     };
-   
-    // Koinzidenz neurons
-    let cfg_k = net.create_neuron_config(NeuronConfig {
-        arp: us(500), // 0.5 ms = 500 us
-        tau_m: 0.04,
-        tau_r: 0.5,
+    let output_neuron_template = NeuronConfig {
+        arp: ms(0),
+        tau_m: 0.0,
+        tau_r: 0.0,
         weight_r: 0.0,
-        threshold: 1.1,
-        record: None,
-    });
+        threshold: 0.609375,
+        record: Some("output"),
+    };
 
-    // Input neurons
-    let cfg_i1 = net.create_neuron_config(NeuronConfig {
+    let cfg_input0 = net.create_neuron_config(NeuronConfig {
+        record: Some("input0"),
+        ..input_neuron_template
+    });
+    let cfg_input1 = net.create_neuron_config(NeuronConfig {
         record: Some("input1"),
         ..input_neuron_template
     });
-    let cfg_i2 = net.create_neuron_config(NeuronConfig {
-        record: Some("input2"),
-        ..input_neuron_template
+    let cfg_output0 = net.create_neuron_config(NeuronConfig {
+        record: Some("output0"),
+        ..output_neuron_template
+    });
+    let cfg_output1 = net.create_neuron_config(NeuronConfig {
+        record: Some("output1"),
+        ..output_neuron_template
+    });
+    let cfg_output2 = net.create_neuron_config(NeuronConfig {
+        record: Some("output2"),
+        ..output_neuron_template
+    });
+    let cfg_innerinp = net.create_neuron_config(NeuronConfig {
+        arp: ms(1),
+        tau_m: 0.0,
+        tau_r: 0.0,
+        weight_r: 0.0,
+        threshold: 0.59375,
+        record: None,
+    });
+    // Koinzidenz neurons
+    let cfg_k = net.create_neuron_config(NeuronConfig {
+        arp: us(500), // 0.5 ms = 500 us
+        tau_m: 0.046875,
+        tau_r: 0.0,
+        weight_r: 0.0,
+        threshold: 1.09375,
+        record: None,
     });
 
-    let inp1 = net.create_neuron(cfg_i1);
-    let inp2 = net.create_neuron(cfg_i2);
+    let n_input0 = net.create_neuron(cfg_input0);
+    let n_input1 = net.create_neuron(cfg_input1);
+    let n_output0 = net.create_neuron(cfg_output0);
+    let n_output1 = net.create_neuron(cfg_output1);
+    let n_output2 = net.create_neuron(cfg_output2);
+    let n_innerinp0 = net.create_neuron(cfg_innerinp);
+    let n_innerinp1 = net.create_neuron(cfg_innerinp);
+    let n_k0 = net.create_neuron(cfg_k);
+    let n_k1 = net.create_neuron(cfg_k);
+    let n_k2 = net.create_neuron(cfg_k);
 
-    net.create_synapse(inp1, Synapse {delay: us(160), weight: 1.0, post_neuron: inp2});
+    net.create_synapse(n_input0, Synapse {delay: us(0), weight: 1.0, post_neuron: n_innerinp0});
+    net.create_synapse(n_input1, Synapse {delay: us(0), weight: 1.0, post_neuron: n_innerinp1});
+    net.create_synapse(n_k0, Synapse {delay: us(0), weight: 1.0, post_neuron: n_output0});
+    net.create_synapse(n_k1, Synapse {delay: us(0), weight: 1.0, post_neuron: n_output1});
+    net.create_synapse(n_k2, Synapse {delay: us(0), weight: 1.0, post_neuron: n_output2});
 
-    // Fill event queue with events
-    for i in 1..100 {
-        let ev = Event {
-            time:   ms(i) / 10,   
-            weight: 0.6,
-            target: inp1
-        };
+    net.create_synapse(n_innerinp0, Synapse {delay: us(625), weight: 1.0, post_neuron: n_k0});
+    net.create_synapse(n_innerinp0, Synapse {delay: us(15), weight: 1.0, post_neuron: n_k1}); // 0.015625
+    net.create_synapse(n_innerinp0, Synapse {delay: us(0), weight: 1.0, post_neuron: n_k2});
 
-        net.add_event(ev);
-    }
+    net.create_synapse(n_innerinp1, Synapse {delay: us(0), weight: 1.0, post_neuron: n_k0});
+    //net.create_synapse(n_innerinp1, Synapse {delay: us(0), weight: 1.0, post_neuron: n_k1});
+    net.create_synapse(n_innerinp1, Synapse {delay: us(15), weight: 1.0, post_neuron: n_k1});
+    net.create_synapse(n_innerinp1, Synapse {delay: us(593), weight: 1.0, post_neuron: n_k2});
+
+    net.add_spike_train_float_ms(n_input0, 1.0, &spikes_input_0);
+    net.add_spike_train_float_ms(n_input1, 1.0, &spikes_input_1);
 
     net.simulate();
 }
